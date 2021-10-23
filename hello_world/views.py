f93c63748538a763f78638dfaa95df85b6ed486f
Kasia-Ros <katarzynarosol6@gmail.com>
2021-10-23 15:22:55 +0000

    Zmiany w Views.py

--- hello_world/views.py
+++ hello_world/views.py
@@ -2,17 +2,23 @@ from hello_world import app
 from hello_world.formater import get_formatted
 from hello_world.formater import SUPPORTED, PLAIN
 from flask import request
+from flask import Flask
 
-moje_imie = "Natalia"
+moje_imie = "Kasia"
 msg = "Hello World!"
 
 @app.route('/')
 def index():
     output = request.args.get('output')
+    name = request.args.get('name')
+
+    if not name:
+        name = moje_imie
     if not output:
         output = PLAIN
-    return get_formatted(msg, moje_imie,
+    return get_formatted(msg, name,
                          output.lower())
+
 
 @app.route('/outputs')
 def supported_output():
